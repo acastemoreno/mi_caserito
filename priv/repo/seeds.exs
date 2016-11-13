d6b6ca629e52bb996d5756f47f72e38c1fe9ee8e
acastemoreno <acastemoreno@gmail.com>
2016-11-13 00:10:53 -0500

    httposion

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+%HTTPotion.Response{body: body} = HTTPotion.post("http://sistemas.minag.gob.pe/sisap/portal2/ciudades/generos/filtrarPorRegion", region: 150000, ajax: true)
+text = Enum.join(for <<c::utf8 <- body>>, do: <<c::utf8>>)
+fun = fn([_, number, nombre])-> {number, nombre}end
+Regex.scan(~r/id=CHK_(\d+)_CHK><a id=genCheck.\d+><\/a>([\w\s]+)<\/label>/, text)
+|> Enum.map(&fun.(&1))
+|> IO.inspect
